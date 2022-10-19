da0bcf2c0eefee63ba5f5b627d9fdbc2f74a72fb
GBro <george@teemill.com>
2022-10-19 19:58:47 +0100

    feat: events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,15 +1,43 @@
 class EventsController < ApplicationController
+  # this is the access point handler
   def track
-    e = Event.new(track_validated_params)
-    e.save
+    # e = Event.new(track_validated_params)
+    # e.save
 
-    render json: e
+    # render json: e
+    render json: uri_parts
   end
 
+  # perform parameter validation
   def track_validated_params
     {
       :name => params.require(:name),
       :ref => params.require(:ref)
     }
+  end
+
+  def uri_parts
+    require 'uri'
+
+    uri = URI.parse(params.require(:referer))
+    
+    parts = {
+      :host => uri.host,
+      :path => uri.path,
+      :query => uri.query,
+      :pathPart1 => nil,
+      :pathPart2 => nil,
+      :pathPart3 => nil 
+    }
+
+    # fill the pathPart properties with available data
+    path_parts = uri.path.split('/').reject(&:empty?)
+
+    i = 0
+    while i < [path_parts.length, 3].min do
+      parts["pathPart#{i + 1}"] = path_parts[i]
+      i += 1
+    end
+    parts
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_18_184830) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_18_182237) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "events", force: :cascade do |t|
-    t.string "name"
-    t.string "ref"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
 end
