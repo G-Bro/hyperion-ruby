bb1812fce1d30318e94f881e096d8deab3ff1851
GBro <george@teemill.com>
2022-11-02 20:56:02 +0000

    feat: convert home.vue to composition

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,7 +2,7 @@ class StaticController < ApplicationController
   def index
     render inertia: 'Home',
       props: {
-        
+        user: current_user
       }
   end
 end
--- app/frontend/Pages/Home.vue
+++ app/frontend/Pages/Home.vue
@@ -1,26 +1,27 @@
+<script setup>
+import Layout from '~/Layouts/Dashboard.vue';
+import HypButton from '~/components/HypButton.vue';
+
+import { ref } from 'vue';
+
+defineProps({
+  user: Object,
+});
+
+const swag = ref('yes')
+</script>
+
 <template>
   <Layout>
     <template #default>
       <hyp-button href="/test">Content</hyp-button>
-      <hyp-button href="/users/sign_up">Sign up</hyp-button>
-      <hyp-button href="/users/sign_in">Login</hyp-button>
+      <template v-if="!user">
+        <hyp-button href="/users/sign_up">Sign up</hyp-button>
+        <hyp-button href="/users/sign_in">Login</hyp-button>
+      </template>
+      <template v-else>
+        <h2>You are logged in :)</h2>
+      </template>
     </template>
   </Layout>
 </template>
-
-<script>
-import Layout from '~/Layouts/Dashboard.vue';
-import HypButton from '~/components/HypButton.vue';
-
-export default {
-  components: {
-    Layout,
-    HypButton,
-  },
-  data() {
-    return {
-      swag: 'yes',
-    };
-  },
-}
-</script>
